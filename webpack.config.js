9462059d28e4bbb30514477e7228c368fc92539e
Mikael Grön <skaramicke@gmail.com>
2024-01-30 08:51:01 +0100

    Move profile photos to static dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,57 @@
-const CopyPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
-const DataPacker = require('./plugins/data-packer')
-const FaviconGenerator = require('./plugins/favicon-generator')
-const PhotosPacker = require('./plugins/photos-packer')
-const StaticTemplateRenderer = require('./plugins/static-template-renderer')
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const DataPacker = require("./plugins/data-packer");
+const FaviconGenerator = require("./plugins/favicon-generator");
+const PhotosPacker = require("./plugins/photos-packer");
+const StaticTemplateRenderer = require("./plugins/static-template-renderer");
 
-const publicPath = path.join(__dirname, 'public')
+const publicPath = path.join(__dirname, "public");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
+  mode: "development",
+  entry: "./src/app.js",
   output: {
     path: publicPath,
-    filename: 'app.[contenthash].js',
-    clean: true
+    filename: "app.[contenthash].js",
+    clean: true,
   },
   module: {
     rules: [
       {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         test: /\.js[x]?$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
   plugins: [
     new PhotosPacker(),
     new DataPacker(),
     new FaviconGenerator(),
     new HtmlWebpackPlugin({
-      template: './src/static_templates/index.html',
+      template: "./src/static_templates/index.html",
     }),
     new WebpackManifestPlugin({}),
     new CopyPlugin({
       patterns: [
-        { from: './src/admin', to: 'admin' }
-      ]
-    })
+        { from: "./src/admin", to: "admin" },
+        { from: "./src/static", to: "static" },
+      ],
+    }),
   ],
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   devServer: {
-    host: 'local.red',
+    host: "local.red",
     static: {
-      directory: publicPath
+      directory: publicPath,
     },
-    watchFiles: 'src/**/*',
+    watchFiles: "src/**/*",
     hot: true,
-  }
-}
+  },
+};
